2075bacfcbc420e3ca7d873cb7a69178559f74bb
Juampa09 <juanaristi.david9@gmail.com>
2025-08-21 07:32:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title ("Mi primera app")
 from PIL import image 
 
 
-st.header("En este espacio comienzo a crear mis apps para interfaces multimodales.")}
+st.header("En este espacio comienzo a crear mis apps para interfaces multimodales.")
 st.write("Fácilmente puedo realizar backend y frontend.")
 image = Image.open('Escarabajo_de_Blue_Beetle.jpg')
 st.image(image, caption = 'Interfaces Multimodales')
