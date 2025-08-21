f4e79d3f9701f0f324992a7c9ba29f9956c4937a
Juampa09 <juanaristi.david9@gmail.com>
2025-08-21 07:38:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ st.header("En este espacio comienzo a crear mis apps para interfaces multimodale
 st.write("Fácilmente puedo realizar backend y frontend.")
 image = Image.open('Escarabajo_de_Blue_Beetle.jpg')
 st.image(image, caption = 'Interfaces Multimodales')
-texto = st.texto_input('Escribe algo', 'Mi texto')
+texto = st.text_input('Escribe algo', 'Mi texto')
 st.write('El texto escrito es ', texto)
