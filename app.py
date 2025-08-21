3819ba825e8984c457a1602321cb1b8f4767821f
Juampa09 <juanaristi.david9@gmail.com>
2025-08-21 07:30:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ st.title ("Mi primera app")
 
 from PIL import image 
 
-image = Image.open()
+image = Image.open('Escarabajo_de_Blue_Beetle.jpg')
+st.image(image, caption = 'Interfaces Multimodales')
